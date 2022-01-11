6bba314840aa33458dd76290e6bb0c9d2bcb043c
lemuretc <69191136+lemuretc@users.noreply.github.com>
2022-01-11 15:28:46 +1100

    Update index.php

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< HEAD
   print "This is versioz 2 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
-=======
-  print "This is version 2 of the app. I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
->>>>>>> 07190fa3a7bd5e2130eafcc613e00eb05d612209
 ?>
